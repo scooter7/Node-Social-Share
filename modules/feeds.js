180689995e26cf24fa18c070afd51cb1a1cafe6f
scooter7 <scooter.vineburgh@gmail.com>
2017-03-06 13:24:28 -0700

    Update feeds.js

--- modules/feeds.js
+++ modules/feeds.js
@@ -1,13 +1,11 @@
 module.exports = [
-  'http://feeds.feedburner.com/CssTricks',
-  'http://www.smashingmagazine.com/feed',
-  'https://feeds.feedblitz.com/scotch_io',
-  'https://feeds.feedburner.com/24ways',
-  'https://speckyboy.com/feed/',
-  'http://blog.teamtreehouse.com/feed',
-  'http://webdesignledger.com/feed',
-  'https://code.tutsplus.com/posts.atom',
-  'https://www.codementor.io/blog/feed',
-  'https://blog.hashnode.com/rss/',
-  'https://www.toptal.com/developers/blog.rss'
+  'http://www.chronicle.com/section/News/6/rss',
+  'http://www.chronicle.com/section/Administration/16/rss',
+  'http://www.chronicle.com/section/Technology/30/rss',
+  'http://feeds.feedburner.com/FeaturedBlogPosts-DataScienceCentral',
+  'http://feeds.feedburner.com/socialmediatoday_allposts',
+  'https://www.insidehighered.com/news/feed',
+  'http://feeds.feedburner.com/bloghighed',
+  'http://www.huffingtonpost.com/feeds/verticals/college/index.xml',
+  'http://www.changinghighereducation.com/atom.xml' 
 ];
